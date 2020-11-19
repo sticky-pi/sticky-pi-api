831db6a0634db128dcf117270b56ac8bcfe2edd2
Quentin Geissman <qgeissmann@gmail.com>
2020-11-19 10:44:29 +0200

    another attempt to try getting RTD to install sphinx extention

--- src/setup.py
+++ src/setup.py
@@ -27,7 +27,7 @@ setup(
                       'itsdangerous'],
     extras_require={
         'test': ['nose', 'pytest', 'codecov'],
-        'docs': ['sphinx-autodoc-typehints', 'sphinx', 'sphinx_rtd_theme', 'recommonmark', 'mock']
+        'docs': ['sphinx_autodoc_typehints', 'sphinx', 'sphinx_rtd_theme', 'recommonmark', 'mock']
     },
     test_suite='nose.collector'
 )
