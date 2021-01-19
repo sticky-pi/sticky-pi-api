ff38170a81a6872530b2d90b4122e637b9a79a37
Quentin Geissman <qgeissmann@gmail.com>
2021-01-19 10:00:29 +0200

    uid in docker++

--- server/ml/uid.py
+++ server/ml/uid.py
@@ -1,11 +1,14 @@
 
 import os
 from sticky_pi_api.client import RemoteClient
-from sticky_pi_ml.insect_tuboid_classifier.ml_bundle import ClientMLBundle
-from sticky_pi_ml.insect_tuboid_classifier.predictor import Predictor
+from sticky_pi_ml.universal_insect_detector.ml_bundle import ClientMLBundle
+from sticky_pi_ml.universal_insect_detector.predictor import Predictor
 
 
-BUNDLE_NAME = 'insect-tuboid-classifier'
+BUNDLE_NAME = 'universal-insect-detector'
+
+bundle_dir = os.path.join(os.environ["BUNDLE_ROOT_DIR"], BUNDLE_NAME)
+
 if __name__ == '__main__':
 
     client = RemoteClient(os.path.join(os.environ["LOCAL_CLIENT_DIR"], 'client'),
@@ -13,10 +16,10 @@ if __name__ == '__main__':
                        os.environ["UID_USER"],
                        os.environ["UID_PASSWORD"], protocol=os.environ["RSHINY_UPSTREAM_PROTOCOL"], port=int(os.environ["RSHINY_UPSTREAM_PORT"]))
 
-    ml_bundle = ClientMLBundle(os.environ["BUNDLE_ROOT_DIR"],
+    ml_bundle = ClientMLBundle(bundle_dir,
                                client,
                                device="cpu",
-                               cache_dir=os.path.join(os.environ["BUNDLE_ROOT_DIR"], "/.cache"))
+                               cache_dir=os.path.join(bundle_dir, "/.cache"))
     ml_bundle.sync_remote_to_local()
     predictor = Predictor(ml_bundle)
     predictor.predict_client(device="%", start_datetime="2020-01-01_00-00-00", end_datetime="2100-01-01_00-00-00")
