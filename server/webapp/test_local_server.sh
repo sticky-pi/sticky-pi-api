2e65ab152ad3e3e0a23dd82d75208025c78e49f8
Wei Xie <wei.tom.xie@gmail.com>
2022-06-22 16:03:09 -0700

    trying to set up own user login

--- server/webapp/test_local_server.sh
+++ server/webapp/test_local_server.sh
@@ -1,5 +1,4 @@
 export STICKY_PI_TESTING_RSHINY_BYPASS_LOGGIN=FALSE
-export STICKY_PI_TESTING_USER=admin
 
 export API_ROOT_URL=api.sticky-pi.com
 export API_PROTOCOL=https
@@ -10,9 +9,21 @@ export RSHINY_UPSTREAM_PORT=443
 export RSHINY_UPSTREAM_PROTOCOL=https
 
 #defined elsewhere
+#export STICKY_PI_TESTING_USER=admin
 #export STICKY_PI_TESTING_PASSWORD=**************
-source /tmp/.spi_webapp.secret.env
+#source /tmp/.spi_webapp.secret.env
+
+export STICKY_PI_TESTING_USER="wei"
+export STICKY_PI_TESTING_PASSWORD="cbsfbyuekiw678sdhw"
+#realpath /home/mxie/StickyPi/api_login.env
+#source /home/mxie/StickyPi/api_login.env
+#echo "$user"
+#export STICKY_PI_TESTING_USER=$user
+#echo "$pass"
+#export STICKY_PI_TESTING_PASSWORD=$pass
 
 export STICKY_PI_TESTING_RSHINY_AUTOLOGIN=TRUE
 export STICKY_PI_TESTING_RSHINY_USE_MOCK_API=FALSE
+# manually set time zone, only for Windows Subsystem for Linux because no real systemd --> can't run timedatectl
+export TZ="America/Vancouver"
 R -e "shiny::runApp('sticky_pi_app', port=4434)"
